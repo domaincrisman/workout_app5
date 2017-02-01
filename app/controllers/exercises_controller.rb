54e43ebb9777ea22658bac83317353afca2298dd
crisman <cmanzanor@hotmail.com>
2017-02-01 21:25:24 +0000

    Add jquery ui datepicker

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -17,7 +17,7 @@ class ExercisesController < ApplicationController
       flash[:notice] = "Exercise has been created"
       redirect_to [current_user, @exercise]
     else
-      flash[:notice] = "Exercise has not been created"
+      flash.now[:notice] = "Exercise has not been created"
       render :new
     end
   end
