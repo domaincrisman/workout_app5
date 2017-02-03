4bb2d88091fb5886d9b3d851bd80ff7a31c4d186
crisman <cmanzanor@hotmail.com>
2017-02-03 13:03:18 +0000

    Implement editing exercise

--- spec/features/exercises/editing_exercises_spec.rb
+++ spec/features/exercises/editing_exercises_spec.rb
@@ -3,11 +3,10 @@ require "rails_helper"
 RSpec.feature "Editing Exercise" do
    
   before do
-    @john = User.create!(email: "owner@example.com", password: "password")
-    
-    @owner_exercise = @owner.exercises.create(duration_in_min: 48,
-                                  workout: "My body building activity",
-                                  workout_date: Date.today)
+    @owner = User.create!(email: "owner@example.com", password: "password")
+    @owner_exercise = @owner.exercises.create!(duration_in_min: 48,
+                                                workout: "My body building activity",
+                                                workout_date: Date.today)                              
     login_as(@owner)
     
   end
@@ -18,8 +17,10 @@ RSpec.feature "Editing Exercise" do
     
     click_link "My Lounge"
     
-    path = "users/#{@owner.id}/exercises/#{@owner_exercise.id}/edit"
-    link = "a[href= \'#{path}\']"
+    path = "/users/#{@owner.id}/exercises/#{@owner_exercise.id}/edit"
+    
+    link = "a[href=\'#{path}\']"
+    
     find(link).click
     
     fill_in "Duration", with: 45
