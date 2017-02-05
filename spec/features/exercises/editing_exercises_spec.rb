bbc0832e974b1372d4d779cbe4a2f0195d9e54dc
crisman <cmanzanor@hotmail.com>
2017-02-05 12:00:26 +0000

    Implement Destroy action

--- spec/features/exercises/editing_exercises_spec.rb
+++ spec/features/exercises/editing_exercises_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.feature "Editing Exercise" do
    
   before do
-    @john = User.create!(email: "owner@example.com", password: "password")
+    @owner = User.create!(email: "owner@example.com", password: "password")
     
     @owner_exercise = @owner.exercises.create(duration_in_min: 48,
                                   workout: "My body building activity",
@@ -18,8 +18,8 @@ RSpec.feature "Editing Exercise" do
     
     click_link "My Lounge"
     
-    path = "users/#{@owner.id}/exercises/#{@owner_exercise.id}/edit"
-    link = "a[href= \'#{path}\']"
+    path = "/users/#{@owner.id}/exercises/#{@owner_exercise.id}/edit"
+    link = "a[href=\'#{path}\']"
     find(link).click
     
     fill_in "Duration", with: 45
